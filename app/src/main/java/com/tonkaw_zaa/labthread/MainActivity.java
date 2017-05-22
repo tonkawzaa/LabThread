7ee2cc55c0decf31dcbf5f134495dc3e61bbde04
DESKTOP-59QGTIT\Tonkaw_Zaa <tonkaw.zaa@gmail.com>
2017-05-22 21:16:36 +0700

    Use Looper&Handler

--- app/src/main/java/com/tonkaw_zaa/labthread/MainActivity.java
+++ app/src/main/java/com/tonkaw_zaa/labthread/MainActivity.java
@@ -1,5 +1,8 @@
 package com.tonkaw_zaa.labthread;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -8,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     int counter;
     TextView tvCounter;
+    Handler handler;
 
     Thread thread;
     @Override
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         tvCounter = (TextView) findViewById(R.id.tvCounter);
 
         // Thread Method 1 : Thread
+        /*
         thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -42,6 +47,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        thread.start();
+        */
+
+        // Thread Method 2 : Thread with Handler
+        handler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                // Run in Main Thread
+                tvCounter.setText(msg.arg1+ "");
+            }
+        };
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Run in background
+                for (int i = 0 ; i<100; i++)
+                {
+                    counter++;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+
+                    Message msg = new Message();
+                    msg.arg1 = counter;
+                    handler.sendMessage(msg);
+
+                }
+            }
+        });
+
         thread.start();
     }
 
